cd0e609c717d151168af4b960aa92fb6f0df15bb
Alexandre LOUBEAU <loubeau.alexandre3@gmail.com>
2022-12-12 12:08:55 +0100

    [NC] Entity / Migrations Making. SETUP ENVIRONMENT

--- migrations/Version20221212110614.php
+++ migrations/Version20221212110614.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20221212105143 extends AbstractMigration
+final class Version20221212110614 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -23,6 +23,7 @@ final class Version20221212105143 extends AbstractMigration
         $this->addSql('CREATE TABLE files (id INT AUTO_INCREMENT NOT NULL, user_id_id INT NOT NULL, name VARCHAR(255) NOT NULL, path LONGTEXT NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_63540599D86650F (user_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE files_files_categories (files_id INT NOT NULL, files_categories_id INT NOT NULL, INDEX IDX_CDC471FDA3E65B2F (files_id), INDEX IDX_CDC471FD5DAD7AB8 (files_categories_id), PRIMARY KEY(files_id, files_categories_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE files_categories (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE files ADD CONSTRAINT FK_63540599D86650F FOREIGN KEY (user_id_id) REFERENCES users (id)');
         $this->addSql('ALTER TABLE files_files_categories ADD CONSTRAINT FK_CDC471FDA3E65B2F FOREIGN KEY (files_id) REFERENCES files (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE files_files_categories ADD CONSTRAINT FK_CDC471FD5DAD7AB8 FOREIGN KEY (files_categories_id) REFERENCES files_categories (id) ON DELETE CASCADE');
@@ -37,5 +38,7 @@ final class Version20221212105143 extends AbstractMigration
         $this->addSql('DROP TABLE files');
         $this->addSql('DROP TABLE files_files_categories');
         $this->addSql('DROP TABLE files_categories');
+        $this->addSql('DROP TABLE users');
+        $this->addSql('DROP TABLE messenger_messages');
     }
 }
