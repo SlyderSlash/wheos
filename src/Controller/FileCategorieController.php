78063dfe763b234fd145263724b00fb511320593
28010-55-08 <28010-55-08@exchange.ad.afpanet>
2022-12-13 13:49:32 +0100

    [NC] Adding more controller to the FileScATEGORIES ENTITY

--- src/Controller/FileCategorieController.php
+++ src/Controller/FileCategorieController.php
@@ -10,18 +10,18 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/files/categories')]
-class FilesCategoriesController extends AbstractController
+#[Route('/file/categorie')]
+class FileCategorieController extends AbstractController
 {
-    #[Route('/', name: 'app_files_categories_index', methods: ['GET'])]
+    #[Route('/', name: 'app_file_categorie_index', methods: ['GET'])]
     public function index(FilesCategoriesRepository $filesCategoriesRepository): Response
     {
-        return $this->render('files_categories/index.html.twig', [
+        return $this->render('file_categorie/index.html.twig', [
             'files_categories' => $filesCategoriesRepository->findAll(),
         ]);
     }
 
-    #[Route('/new', name: 'app_files_categories_new', methods: ['GET', 'POST'])]
+    #[Route('/new', name: 'app_file_categorie_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $filesCategory = new FilesCategories();
@@ -32,24 +32,24 @@ class FilesCategoriesController extends AbstractController
             $entityManager->persist($filesCategory);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_files_categories_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_file_categorie_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->renderForm('files_categories/new.html.twig', [
+        return $this->renderForm('file_categorie/new.html.twig', [
             'files_category' => $filesCategory,
             'form' => $form,
         ]);
     }
 
-    #[Route('/{id}', name: 'app_files_categories_show', methods: ['GET'])]
+    #[Route('/{id}', name: 'app_file_categorie_show', methods: ['GET'])]
     public function show(FilesCategories $filesCategory): Response
     {
-        return $this->render('files_categories/show.html.twig', [
+        return $this->render('file_categorie/show.html.twig', [
             'files_category' => $filesCategory,
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_files_categories_edit', methods: ['GET', 'POST'])]
+    #[Route('/{id}/edit', name: 'app_file_categorie_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, FilesCategories $filesCategory, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(FilesCategoriesType::class, $filesCategory);
@@ -58,16 +58,16 @@ class FilesCategoriesController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_files_categories_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_file_categorie_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->renderForm('files_categories/edit.html.twig', [
+        return $this->renderForm('file_categorie/edit.html.twig', [
             'files_category' => $filesCategory,
             'form' => $form,
         ]);
     }
 
-    #[Route('/{id}', name: 'app_files_categories_delete', methods: ['POST'])]
+    #[Route('/{id}', name: 'app_file_categorie_delete', methods: ['POST'])]
     public function delete(Request $request, FilesCategories $filesCategory, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$filesCategory->getId(), $request->request->get('_token'))) {
@@ -75,6 +75,6 @@ class FilesCategoriesController extends AbstractController
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('app_files_categories_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('app_file_categorie_index', [], Response::HTTP_SEE_OTHER);
     }
 }
