5b91a666ecc2f374069e8abfb39f02288b882463
Formation2022 <rachid.abbou@gmail.com>
2022-12-16 18:17:17 +0000

    NC css presque fini, puis un peu de js

--- public/js/forum.js
+++ public/js/forum.js
@@ -1,27 +1,22 @@
 
-let btnAddSujet = document.getElementById('addsujet');
-let btnAddresponse = document.getElementById('addresponse');
-
-
-
-btnAddSujet.addEventListener('click', () => {
-let icon = document.getElementById('icon');
+let btnAddSujet = document.getElementById('addsujet').addEventListener('click', () => {
+    let icon = document.getElementById('icon');
   
     if (icon.classList.contains('bi-plus-circle-fill')) {
         icon.classList.replace('bi-plus-circle-fill','bi-dash-circle-fill');
     }else if (icon.classList.contains('bi-dash-circle-fill')) {
         icon.classList.replace('bi-dash-circle-fill','bi-plus-circle-fill');
     }
-    alert('click bouton ajouter une discussion')
 });
 
-btnAddresponse.addEventListener('click', () => {
-    let icon = document.getElementById('icone');
+
+let btnAddresponse = document.getElementById('add').addEventListener('click', () => {
+    let icon = document.getElementById('circle');
       
- /*       if (icon.classList.contains('bi-plus-circle-fill')) {
-            icon.classList.replace('bi-plus-circle-fill','bi-dash-circle-fill');
-        }else{
-            icon.classList.replace('bi-dash-circle-fill','bi-plus-circle-fill');
-        }*/
-        alert('click bouton ajouter une réponse')
-    });
+    if (icon.classList.contains('bi-plus-circle-fill')) {
+        icon.classList.replace('bi-plus-circle-fill','bi-dash-circle-fill');
+    }else if (icon.classList.contains('bi-dash-circle-fill')){
+        icon.classList.replace('bi-dash-circle-fill','bi-plus-circle-fill');
+    }
+});
+
