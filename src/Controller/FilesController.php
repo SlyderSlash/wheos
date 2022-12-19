b276ae48ac387f8340852df089c5e0377940d362
28010-55-02 <28010-55-02@exchange.ad.afpanet>
2022-12-19 16:54:58 +0100

    [NC] Modifications Pages Twig

--- src/Controller/FilesController.php
+++ src/Controller/FilesController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/files')]
+#[Route('/file')]
 class FilesController extends AbstractController
 {
     #[Route('/', name: 'app_files_index', methods: ['GET'])]
