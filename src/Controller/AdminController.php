cd192ded0e0abac455047d230aac017d70d1dbba
Formation2022 <rachid.abbou@gmail.com>
2022-12-21 18:53:27 +0000

    NC partie admin des categories en cours

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Categories;
 use App\Entity\Users;
+use App\Form\CategoriesType;
 use App\Form\EditUserType;
+use App\Repository\CategoriesRepository;
 use App\Repository\UsersRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -45,6 +48,63 @@ class AdminController extends AbstractController
         
         return $this->render('admin/edituser.html.twig', [
             'userForm' => $form->createView(),
+        ]);
+    }
+
+    #[Route("/categories", name: "Categories")]
+    public function showCategory(CategoriesRepository $categoriesRepository)
+    {
+        return $this->render('admin/categories/categories.html.twig', [
+            'categories' => $categoriesRepository->findAll(),
+        ]);
+    }
+
+
+    #[Route("/categories/add", name: "addCategory")]
+    public function addCategory(Request $request)
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+
+        $category = new Categories;
+
+        $form = $this->createForm(CategoriesType::class,$category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this-> ManagerRegistry::getManager();   
+            $entityManager->persist($category);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'categorie ajoutée');
+            return $this->redirectToRoute('categories');
+        }
+        return $this->render('admin/categories/addCategories.html.twig', [
+            'categoryform' => $form->createView(),
+        ]);
+    }
+
+
+    #[Route("/categories/edit/{id}", name: "editCategory")]
+    public function editCategory(Categories $categories, Request $request)
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        $form = $this->createForm(CategoriesType::class, $categories);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this-> ManagerRegistry::getManager();   
+            $entityManager->persist($categories);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'categorie modifiée');
+            return $this->redirectToRoute('categories');
+        }
+
+
+        return $this->render('admin/categories/editCategories.html.twig', [
+            'categoriesForm' => $form->createView(),
         ]);
     }
 }
--- src/Form/ForumType.php
+++ src/Form/ForumType.php
@@ -2,30 +2,31 @@
 
 namespace App\Form;
 
-use App\Entity\ForumMessages;
 use App\Entity\Forums;
 use App\Entity\Users;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Entity\Categories;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ForumMessagesType extends AbstractType
+class ForumType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('content')
+            ->add('title')
             ->add('createdAt')
+            ->add('isResolved')
+            ->add('isClosed')
             ->add('user',EntityType::class,['class' => Users::class])
-            ->add('forum',EntityType::class,['class' => Forums::class])
+            ->add('category',EntityType::class,['class' => Categories::class])
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => ForumMessages::class,
+            'data_class' => Forums::class,
         ]);
     }
 }
